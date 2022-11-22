7ab7e7c4caa952140a9b9dfae9e229a402de639a
Chet Jogia <chetjogia@gmail.com>
2022-11-22 10:03:59 +0000

    initialised app and added dummy data

---
+++ utellum/TestData/index.js
@@ -0,0 +1,172 @@
+//Bootcamper and Topics Dummy Data
+
+export const bootcampers = [
+    {
+        id: 1,
+        first_name: "Alex",
+        last_name: "Chappell",
+        password: "password1",
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbdogR0N8StxveW_74HcMvZcOE5xjSDZIr7juvN43eMeX04WC5mz8i9dN27LoXTBIBeYU&usqp=CAU"
+    },
+    {
+        id: 2,
+        first_name: "Chet",
+        last_name: "Jogia",
+        password: "password2",
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbdogR0N8StxveW_74HcMvZcOE5xjSDZIr7juvN43eMeX04WC5mz8i9dN27LoXTBIBeYU&usqp=CAU"
+    },
+    {
+        id: 3,
+        first_name: "Luke",
+        last_name: "Adams",
+        password: "password3",
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbdogR0N8StxveW_74HcMvZcOE5xjSDZIr7juvN43eMeX04WC5mz8i9dN27LoXTBIBeYU&usqp=CAU"
+    },
+    {
+        id: 4,
+        first_name: "Jason",
+        last_name: "Riley",
+        password: "password4",
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbdogR0N8StxveW_74HcMvZcOE5xjSDZIr7juvN43eMeX04WC5mz8i9dN27LoXTBIBeYU&usqp=CAU"
+    }
+]
+
+export const topics = [
+    {
+        id: 1,
+        topic_name: "React"
+    },
+    {
+        id: 2,
+        topic_name: "Node"
+    },
+    {
+        id: 3,
+        topic_name: "SQL"
+    },
+    {
+        id: 4,
+        topic_name: "CSS"
+    },
+    {
+        id: 5,
+        topic_name: "UIUX"
+    },
+    {
+        id: 6,
+        topic_name: "Javascript"
+    },
+    {
+        id: 7,
+        topic_name: "HTML"
+    }   
+
+
+]
+
+
+export const strengths_weaknesses = [
+    {
+        id: 1,
+        bootcamper_ID: 1,
+        topic: 1,
+        strength_weakness: true
+    },
+    {
+        id: 2,
+        bootcamper_ID: 1,
+        topic: 2,
+        strength_weakness: true
+    },
+    {
+        id: 3,
+        bootcamper_ID: 1,
+        topic: 3,
+        strength_weakness: true
+    },
+    {
+        id: 4,
+        bootcamper_ID: 1,
+        topic: 5,
+        strength_weakness: false
+    },
+    {
+        id: 5,
+        bootcamper_ID: 1,
+        topic: 7,
+        strength_weakness: false
+    },
+    {
+        id: 6,
+        bootcamper_ID: 2,
+        topic: 3,
+        strength_weakness: true
+    },
+    {
+        id: 7,
+        bootcamper_ID: 2,
+        topic: 6,
+        strength_weakness: false
+    },
+    {
+        id: 8,
+        bootcamper_ID: 2,
+        topic: 7,
+        strength_weakness: false
+    },
+    {
+        id: 9,
+        bootcamper_ID: 3,
+        topic: 6,
+        strength_weakness: true
+    },
+    {
+        id: 10,
+        bootcamper_ID: 3,
+        topic: 7,
+        strength_weakness: true
+    },
+    {
+        id: 11,
+        bootcamper_ID: 3,
+        topic: 2,
+        strength_weakness: true
+    },
+    {
+        id: 12,
+        bootcamper_ID: 3,
+        topic: 3,
+        strength_weakness: false
+    },
+    {
+        id: 13,
+        bootcamper_ID: 4,
+        topic: 2,
+        strength_weakness: true
+    },
+    {
+        id: 14,
+        bootcamper_ID: 4,
+        topic: 6,
+        strength_weakness: true
+    },
+    {
+        id: 15,
+        bootcamper_ID: 4,
+        topic: 4,
+        strength_weakness: true
+    },
+    {
+        id: 16,
+        bootcamper_ID: 4,
+        topic: 1,
+        strength_weakness: false
+    },
+    {
+        id: 17,
+        bootcamper_ID: 4,
+        topic: 7,
+        strength_weakness: false
+    }
+
+]
