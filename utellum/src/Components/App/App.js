aef4cec4ca77037ce4945042cdb0145d4e08671e
Chet Jogia <chetjogia@gmail.com>
2022-11-22 22:47:30 +0000

    fully functional search bar by all attributes

--- utellum/src/Components/App/App.js
+++ utellum/src/Components/App/App.js
@@ -6,18 +6,39 @@ import Search from '../Search'
 import {useState, useEffect} from 'react'
 import {Routes, Route} from 'react-router-dom'
 import BootcamperProfile from '../BootcamperProfile';
-
+import { strengths_weaknesses } from '../../TestData/index.js';
 
 
 
 function App() {
+  for(let i=0; i<bootcampers.length;i++){
+    let strengthArray=[]
+    let weaknessArray=[]
+    for(let j=0; j<strengths_weaknesses.length; j++){
+      if(strengths_weaknesses[j].bootcamper_ID===bootcampers[i].id && strengths_weaknesses[j].strength_weakness===true){
+        strengthArray.push(strengths_weaknesses[j].topic_name.toLowerCase())
+      }
+      else if(strengths_weaknesses[j].bootcamper_ID===bootcampers[i].id && strengths_weaknesses[j].strength_weakness===false){
+        weaknessArray.push(strengths_weaknesses[j].topic_name.toLowerCase())
+      }
+    }
+    bootcampers[i]["strength"]=strengthArray
+    bootcampers[i]["weakness"]=weaknessArray
+  }
+
   const [bootcamperArray, setBootcamperArray] = useState(bootcampers)
   const [input, setInput] = useState("")
   const [filter, setFilter] = useState("first_name")
 
   useEffect(()=>{
     if(input!==""){
-      let filteredBootcamperArray = bootcamperArray.filter((element)=>{return element[filter].toLowerCase().includes(input)})
+      let filteredBootcamperArray = []
+      if(filter==="first_name"||filter==="last_name"){
+         filteredBootcamperArray = bootcamperArray.filter((element)=>{return element[filter].toLowerCase().includes(input)})
+      }
+      else if(filter==="strength"||filter==="weakness"){
+         filteredBootcamperArray = bootcamperArray.filter((element)=>{return element[filter].some(e=>e.includes(input))})
+      }
       setBootcamperArray(filteredBootcamperArray)
     }
     else{
@@ -29,7 +50,7 @@ function App() {
 
   function inputHandler(event){
     let inputValue = document.querySelector(".input").value
-    setInput(inputValue)
+    setInput(inputValue.toLowerCase())
     setBootcamperArray(bootcampers)
   }
 
