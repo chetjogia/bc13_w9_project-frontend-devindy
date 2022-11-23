9ccf19906bb8a62270e5c8e66b0a87bbfe4fc4ae
Chet Jogia <chetjogia@gmail.com>
2022-11-23 16:58:14 +0000

    front end fully working

--- utellum/src/Components/App/App.js
+++ utellum/src/Components/App/App.js
@@ -12,7 +12,8 @@ import { v4 as uuidv4 } from 'uuid'
 
 function App() {
 
-
+  
+  
   const [bootcamperArray, setBootcamperArray] = useState([])
   const [bootcamperSW, setBootcamperSW]=useState([])
   const [counter,setCounter] = useState(0)
